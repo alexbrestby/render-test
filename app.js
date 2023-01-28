e9bd67c9832a062f7e3a307c06963667402e232a
Alex Leoniuk <alex.brest.by@gmail.com>
2023-01-28 12:30:07 +0300

    feat: Logger middleware add

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+const myLogger = function (req, res, next) {
+  console.log('TimeLog: ', Date.now());
+  next();
+};
+
+app.use(myLogger);
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Main Page')
 })
 
 app.get('/about', (req, res) => {
